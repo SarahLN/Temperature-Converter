c13a4e509362e23e4366611912b9a76145f1166d
SarahLN <noles.sarah@gmail.com>
2018-02-20 18:49:33 -0600

    after 9.5

---
+++ app/src/main/java/com/vogella/android/temperatureconverter/ConverterUtil.java
@@ -0,0 +1,17 @@
+package com.vogella.android.temperatureconverter;
+
+/**
+ * Created by sarah on 2/20/2018.
+ */
+
+public class ConverterUtil {
+    // converts to celsius
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+
+    // converts to fahrenheit
+    public static float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+}
