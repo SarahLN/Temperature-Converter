53d7193861345e857189b01764ad1866093f75b2
SarahLN <noles.sarah@gmail.com>
2018-02-27 19:39:00 -0600

    adding two notification channels

--- app/src/main/java/com/vogella/android/temperatureconverter/NotificationUtil.java
+++ app/src/main/java/com/vogella/android/temperatureconverter/NotificationUtil.java
@@ -1,8 +1,10 @@
 package com.vogella.android.temperatureconverter;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.ContextWrapper;
+import android.graphics.Color;
 
 /**
  * Created by sarah on 2/27/2018.
@@ -16,7 +18,15 @@ public class NotificationUtil extends ContextWrapper {
     public NotificationUtil(Context ctx) {
         super(ctx);
 
-        //NotificationChannel chan1 = new NotificationChannel(PRIMARY_CHANNEL, getString(R.string.))
+        NotificationChannel chan1 = new NotificationChannel(PRIMARY_CHANNEL, getString(R.string.noti_channel_default), NotificationManager.IMPORTANCE_DEFAULT);
+        chan1.setLightColor(Color.BLUE);
+        chan1.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        getManager().createNotificationChannel(chan1);
+
+        NotificationChannel chan2 = new NotificationChannel(SECONDARY_CHANNEL, getString(R.string.noti_channel_second), NotificationManager.IMPORTANCE_HIGH);
+        chan2.setLightColor(Color.GREEN);
+        chan2.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        getManager().createNotificationChannel(chan2);
     }
 }
 
